ccbb3f4310e6a9e841f6bfd7ac2b6e73d2aacebd
Lam Chi Hou <mudora23@gmail.com>
2025-09-25 10:03:03 +0800

    Update index.ts

--- api/src/schedule/index.ts
+++ api/src/schedule/index.ts
@@ -22,7 +22,7 @@ export default async (fastify: TypedFastifyInstance) => {
                 (await import("@/schedule/perfit/fb_leads/process_ad_information.service")).run(fastify)
             ))
 
-            fastify.scheduler.addSimpleIntervalJob(new SimpleIntervalJob({ seconds: 3600, runImmediately: false },
+            fastify.scheduler.addSimpleIntervalJob(new SimpleIntervalJob({ seconds: 600, runImmediately: false },
                 (await import("@/schedule/perfit/fb_leads/process_ad_insights.service")).run(fastify)
             ))
 
@@ -30,7 +30,7 @@ export default async (fastify: TypedFastifyInstance) => {
                 (await import("@/schedule/perfit/google_ads/process_ad_group_information.service")).run(fastify)
             ))
 
-            fastify.scheduler.addSimpleIntervalJob(new SimpleIntervalJob({ seconds: 3600, runImmediately: false },
+            fastify.scheduler.addSimpleIntervalJob(new SimpleIntervalJob({ seconds: 600, runImmediately: false },
                 (await import("@/schedule/perfit/google_ads/process_ad_group_report.service")).run(fastify)
             ))
 
